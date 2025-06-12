380f27cbc7df0882485a7883b968b41499c6b208
Kali Linux <43797010+Rootkalilinuxok@users.noreply.github.com>
2025-06-13 00:36:04 +0200

    Update columns.tsx

--- src/app/(main)/dashboard/default/_components/columns.tsx
+++ src/app/(main)/dashboard/default/_components/columns.tsx
@@ -22,7 +22,7 @@
 */
 
 import { ColumnDef } from "@tanstack/react-table";
-import { DataTableColumnHeader } from "@/components/data-table-column-header";
+import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header";
 import { Checkbox } from "@/components/ui/checkbox";
 import { ReceiptRowActions } from "@/components/table/row-actions";
 
