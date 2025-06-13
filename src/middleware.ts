18b504eec282dd66ef82a8552bfdf35b95dda49c
Kali Linux <43797010+Rootkalilinuxok@users.noreply.github.com>
2025-06-13 02:58:41 +0200

    call auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 import { authMiddleware } from "./middleware/auth-middleware";
 
 export function middleware(req: NextRequest) {
-  // authMiddleware
-  // const response = authMiddleware(req)
-  // if (response) {
-  //   return response
-  // }
-
-  return NextResponse.next();
+  return authMiddleware(req);
 }
 
 export const config = {
