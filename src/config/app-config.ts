4209e4d146869628604907367de7dfce91beb82c
Kali Linux <43797010+Rootkalilinuxok@users.noreply.github.com>
2025-06-14 15:20:23 +0200

    Update app-config.ts

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,7 +1,7 @@
 import packageJson from "../../package.json";
 
 export const APP_CONFIG = {
-  name: "Studio Admin",
+  name: "FrameTech SL",
   version: packageJson.version,
   meta: {
     title: "Studio Admin - Modern Next.js Dashboard Starter Template",
