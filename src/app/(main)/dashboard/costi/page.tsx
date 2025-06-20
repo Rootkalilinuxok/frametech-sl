844fe20f26264f1cbabd14d38aa28ff2ff52e3ad
Kali Linux <43797010+Rootkalilinuxok@users.noreply.github.com>
2025-06-20 21:17:04 +0200

    Update page.tsx

--- src/app/(main)/dashboard/costi/page.tsx
+++ src/app/(main)/dashboard/costi/page.tsx
@@ -11,6 +11,7 @@ export default function Page() {
     <div className="@container/main flex flex-col gap-4 md:gap-6">
       <SectionCards />
       <ChartAreaInteractive />
+      <FileUploadTable />
       <DataTable data={data as unknown as CostiRow[]} />
     </div>
   );
