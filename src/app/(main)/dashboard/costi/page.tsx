41539e12fc8e85a54c95b9758def177a923a6853
Kali Linux <43797010+Rootkalilinuxok@users.noreply.github.com>
2025-06-22 08:16:50 +0200

    Update page.tsx

--- src/app/(main)/dashboard/costi/page.tsx
+++ src/app/(main)/dashboard/costi/page.tsx
@@ -1,5 +1,4 @@
 import { SectionCards } from "./_components/section-cards";
-import { ChartAreaInteractive } from "./_components/chart-area-interactive";
 import { DataTable } from "./_components/data-table";
 import data from "./_components/data.json";
 import type { CostiRow } from "./_components/columns";
@@ -8,7 +7,6 @@ export default function Page() {
   return (
     <div className="@container/main flex flex-col gap-4 md:gap-6">
       <SectionCards />
-      <ChartAreaInteractive />
       <DataTable data={data as unknown as CostiRow[]} />
     </div>
   );
