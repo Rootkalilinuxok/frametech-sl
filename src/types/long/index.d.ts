163296bc32d6031240af6ddd75b2088db4929ed2
Kali Linux <43797010+Rootkalilinuxok@users.noreply.github.com>
2025-06-25 02:47:33 +0200

    Create index.d.ts

---
+++ src/types/long/index.d.ts
@@ -0,0 +1,7 @@
+// src/types/long/index.d.ts
+declare module "long" {
+  // Qui puoi aggiungere dichiarazioni più precise, se ti servono,
+  // ma anche solo questo basta per far sparire l’errore di TS.
+  const Long: any;
+  export default Long;
+}
